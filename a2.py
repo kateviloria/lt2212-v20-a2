973c3800aac66f0791adab97d5d974f7ce2cdd37
Kate Viloria <kate@Kates-MacBook-Pro.local>
2020-06-27 18:13:19 -0400

    more efficient and cleaned comments

--- a2.py
+++ a2.py
@@ -9,13 +9,18 @@ random.seed(42)
 from sklearn.feature_extraction.text import TfidfTransformer
 from nltk.corpus import stopwords
 import string
+import collections
 from sklearn.decomposition import TruncatedSVD
 from sklearn.model_selection import ShuffleSplit
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+# from sklearn.metrics import classification_report 
+        # --> Used at first, excessive info and took more time to process
 
 
 ###### PART 1
@@ -31,82 +36,55 @@ def part1(samples):
 
 def tokenize_text(data):
     """
-    Tokenizes text.
+    Tokenizes text and counts frequencies.
 
     Args:
         samples - 20 newsgroup data
     
     Returns:
-        A list of lists. Each inner list contains all the words that appear in each document (after filtering
-        out punctuation and integers then making all characters in lowercase).
+         A list of dictionaries. Each dictionary represents a document. Within each dictionary, the key is the
+        word in the document and its value is the frequency.
+        [ { word : 4, another_word : 2, a_word : 0}, { word : 0, a_diff_word : 1, what_word : 1}]
 
         AND
 
-        An alphabetized list of all the words that appear in the data. Words only appear once.
+        A list of all the words that appear in the data. Words only appear once.
     """
 
     # from NLTK : words that search engines have been programmed to ignore (ie. pronouns, 'during', 'very')
     stop_words = stopwords.words('english')
 
-
-    # FOR TESTING IN SMALL SAMPLE
     # news_data = newsgroups_all.data
-    # baby_data = news_data[:21]
-    # data_text = baby_data
 
+    # for trial BABY DATA 
+        # baby_data = news_data[:21]
+        # data_text = baby_data
+    
     # tokenize data 
     text_list = [] # should be list of lists, each inner list is text of a file
-    for each_file in data: # CHANGE TO data_text for trial; data for ALL
+    master_list = [] # list of words -> columns for array (will need for indexing)
+    for each_file in data: # CHANGE TO BABY_DATA for trial; NEWS_DATA for actual
         word_list = []
         # make string into list
         string_to_list = each_file.split()
         for every_word in string_to_list:
-            # take out punctuation
-            no_punct = every_word.translate(str.maketrans('','', string.punctuation))
-            if no_punct.isalpha(): # filters out integers and punctuation
-                no_capitals = no_punct.lower() # makes each word into lowercase
+            if every_word.isalpha(): # filters out integers and punctuation
+                no_capitals = every_word.lower() # lowercases all characters of word
                 if no_capitals not in stop_words: # filters through NLTK stop words list
                     word_list.append(no_capitals)
+                    if no_capitals not in master_list:
+                        master_list.append(no_capitals)
         text_list.append(word_list)
-    
-    # make list of all possible words
-    master_list = [] 
+
+    # count freq
+    word_freq = [] # list of dictionaries
     for every_file in text_list:
-        for every_word in every_file:
-            if every_word not in master_list:
-                master_list.append(every_word)
-    master_list.sort() # alphabetise master_list
-
-    return text_list, master_list
-
-
-def count_freq(data):
-    """
-    Count occurrence of each word within each document.
-
-    Args:
-        data - 20 newsgroup data
-
-    Returns:
-        A list of dictionaries. Each dictionary represents a document. Within each dictionary, the key is the
-        word in the document and its value is the frequency.
-        [ { word : 4, another_word : 2, a_word : 0}, { word : 0, a_diff_word : 1, what_word : 1}]
-    """
-
-    words_to_count, master_list = tokenize_text(data)
-
-    # list of dictionaries for word counts
-    word_freq = []
-    for every_file in words_to_count:
-        article_dict = {} # word count for each article
-        for every_word in every_file:
-            if every_word in article_dict.keys():
-                article_dict[every_word] += 1
-            else:
-                article_dict[every_word] = 1
+        article_dict = {} # { WORD : FREQUENCY }, dictionary per text
+        # goes through list per file and uses counter to create a dictionary
+        article_dict = collections.Counter(every_file) 
         word_freq.append(article_dict)
 
-    return  word_freq, master_list
+    return word_freq, master_list
 
 
 def extract_features(samples):
@@ -124,17 +102,17 @@ def extract_features(samples):
 
     print("Extracting features ...")
 
-    # calls count_freq to get dictionary of lists and list of all words
-    word_freq, master_list = count_freq(samples)
+    # calls tokenize text to get word_freq and master_list
+    word_freq, master_list = tokenize_text(samples)
     print('master_list, word_freq done')
     
     # number of rows -> docs
     doc_rows = len(word_freq)
-    print('have doc_rows')
+    print('have doc rows')
 
     # number of columns -> words in entire lexicon
     word_columns = len(master_list)
-    print('have word_columns')
+    print('have word columns')
     
     # make array with 0's for all the words that doesn't appear in doc
     final_array = np.zeros((doc_rows, word_columns))
@@ -144,26 +122,27 @@ def extract_features(samples):
     row_index = 0
     
     for every_article in word_freq:
-        for every_word in master_list:
-            if every_word in every_article.keys():
-                word_index = master_list.index(every_word) # get index from master_list
-                word_count = every_article[every_word] # get word count from dictionary
-                final_array[row_index, word_index] = word_count
-        row_index += 1
-    
+        for every_word in every_article.keys():
+            word_index = master_list.index(every_word) # get index from master_list
+            word_count = every_article[every_word] # get word count from dictionary
+            final_array[row_index, word_index] = word_count # put count in correct index within array
+        row_index += 1 # next doc/row
+
     print('final_array done')
     # print(final_array)
    
-    # CHECK SHAPE OF VEC
-    # shape_of_vec = final_array.shape
-    # print(shape_of_vec)
+    # CHECK SHAPE OF FIRST VEC
+        # shape_of_vec = final_array.shape
+        # print(shape_of_vec)
 
-    # reduced by total word counts per column
-    array_column_sum = np.sum(final_array, axis = 0) # sum of each word (columns) in all of document 
-    array_filter = array_column_sum > 20 # filtered will only have words that appear more than 20 times within entire data set
+    # reduce by total word counts per column
+    array_column_sum = np.sum(final_array, axis = 0) # sum of each word in all documents
+    # filtered will only have words that appear more than 20 times within entire data set
+    array_filter = array_column_sum > 20 
     print('about to filter final array')
-    filtered_array = final_array[:, array_filter]
-    
+    filtered_array = final_array[:, array_filter] # [ROW, COLUMN]
+    # shape_filtered = filtered_array.shape
+
     # consider words that appear in all documents (ie. subject)
         # tfidf = TfidfTransformer(smooth_idf=True, use_idf=True)
         # tfidf_array = tfidf.fit_transform(reduced_array)
@@ -236,7 +215,7 @@ def part3(X, y, clf_id):
 
 
     # evalute model
-    print("Evaluating classcifier ...")
+    print("Evaluating classifier ...")
     evalute_classifier(clf, X_test, y_test)
 
 
@@ -252,13 +231,20 @@ def train_classifer(clf, X, y):
 
 def evalute_classifier(clf, X, y):
     assert is_classifier(clf)
+    # y is y_true
     predicted = clf.predict(X)
     accuracy = accuracy_score(y, predicted)
+    precision = precision_score(y, predicted, average='weighted')
+    recall = recall_score(y, predicted, average='weighted')
+    f1 =  f1_score(y, predicted, average='weighted')
     # calculates precision, recall, F-measure, support (extra)
-    class_report = classification_report(y, clf.predict(X))
+        # class_report = classification_report(y, clf.predict(X))
+        # print(class_report) -> took too long
     print('Accuracy: ', accuracy)
-    print(class_report)
-
+    print('Precision: ', precision)
+    print('Recall: ', recall)
+    print('F-measure: ', f1)
+    
 
 ######
 #DONT CHANGE THIS FUNCTION
@@ -292,7 +278,6 @@ def main(model_id=None, n_dim=False):
     if model_id:
         print("\n------------PART 3-----------")
         part3(X, labels, model_id)
-
 
 
 if __name__ == '__main__':
